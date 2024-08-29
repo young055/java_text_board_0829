35b206eb870b24beb2a59a302071eddb4b95ec5d
young055 <yhun1379@gmail.com>
2024-08-29 20:02:22 +0900

    시작 알림 메세지

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -3,6 +3,8 @@ package com.sbs.java.board;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("==게시판 작동==");
+        System.out.println("게시판을 시작합니다");
+        System.out.println("==게시판 종료==");
     }
 }
