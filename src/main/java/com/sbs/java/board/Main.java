0484d6be209bad859177fae454d8de519ffea7ca
young055 <yhun1379@gmail.com>
2024-08-29 20:18:11 +0900

    exit가 입력받기 전까지 계속 입력

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -6,10 +6,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("==자바 텍스트 게시판 작동==");
-        System.out.print("명령");
-        String cmd =sc.nextLine();
 
-        System.out.printf("입력받은 명령어: %s\n,", cmd);
+        while(true) {
+            System.out.print("명령");
+            String cmd =sc.nextLine();
+
+            if(cmd.equals("exit")){
+                break;
+            }
+            System.out.printf("입력받은 명령어: %s\n,", cmd);
+        }
+
+
+
         System.out.println("==자바 텍스트 게시판 종료==");
+        sc.close();
     }
 }
