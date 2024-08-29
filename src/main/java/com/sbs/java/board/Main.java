7a6e03a14f7a2336182b649696260a594c91c225
young055 <yhun1379@gmail.com>
2024-08-29 20:11:14 +0900

    기본 명령어 처리

--- src/main/java/com/sbs/java/board/Main.java
+++ src/main/java/com/sbs/java/board/Main.java
@@ -1,10 +1,15 @@
 package com.sbs.java.board;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("==자바 텍스트 게시판 작동==");
+        System.out.print("명령");
+        String cmd =sc.nextLine();
 
-        System.out.println("==게시판 작동==");
-        System.out.println("게시판을 시작합니다");
-        System.out.println("==게시판 종료==");
+        System.out.printf("입력받은 명령어: %s\n,", cmd);
+        System.out.println("==자바 텍스트 게시판 종료==");
     }
 }
